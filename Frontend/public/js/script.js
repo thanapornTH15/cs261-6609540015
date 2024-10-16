e1caa6af2e82cc0633c634099f4cdbcc0b57d662
TH <thanapornth2@gmail.com>
2024-10-17 03:36:49 +0700

    updated and cleaned up some files

--- Frontend/public/js/script.js
+++ Frontend/public/js/script.js
@@ -1,23 +1,55 @@
-function submitLogin() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+function togglePassword() {
+    const passwordInput = document.getElementById('password');
+    const eyeIcon = document.getElementById('eye-icon');
 
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text'; // Show the password
+        eyeIcon.src = 'img/eye-open-icon.png';
+        eyeIcon.alt = 'Hide Password';
+    } else {
+        passwordInput.type = 'password'; // Hide the password
+        eyeIcon.src = 'img/eye-close-icon.png';
+        eyeIcon.alt = 'Show Password';
+    }
+}
+
+function submitLogin() {
+    const UserName = document.getElementById('username').value;
+    const PassWord = document.getElementById('password').value;
+    const checkDiv = document.getElementById('check');
+
+    if (!UserName || !PassWord) {
+        checkDiv.innerText = '*Please enter your username & password*';
+        return;
+    }
+    else {
+        document.getElementById('form').style.display = 'none';
+        const resultDiv = document.getElementById('result');
+        resultDiv.style.display = 'block';
+        document.getElementById('status').innerText = 'test';
+        document.getElementById('message').innerText = 
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices tortor quis ex lobortis, vitae sagittis nibh ullamcorper. In semper.';
+    }
+
+    
+    /*
     fetch('/api/auth', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({ UserName, PassWord })
     })
     .then(response => response.json())
     .then(data => {
         document.getElementById('message').innerText = data.message;
     })
     .catch(error => console.error('Error:', error));
+    */
 }
 
 
-
+/*
 function call_REST_API_Hello() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
@@ -34,3 +66,4 @@ function call_REST_API_Hello() {
     })
     .catch(error => console.error('Error:', error));
 }
+*/
